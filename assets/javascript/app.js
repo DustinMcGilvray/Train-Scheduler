948f9ebf6ec2ce24e97a66677be6e5054b573397
DustinMcGilvray <15370964+DustinMcGilvray@users.noreply.github.com>
2018-07-27 11:35:39 -0500

    Files Established.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAK8DT5PJHfnw4_oaniSQx0DjdodBZv9Ns",
+    authDomain: "train-scheduler-project-8916c.firebaseapp.com",
+    databaseURL: "https://train-scheduler-project-8916c.firebaseio.com",
+    projectId: "train-scheduler-project-8916c",
+    storageBucket: "train-scheduler-project-8916c.appspot.com",
+    messagingSenderId: "790464243378"
+  };
+  firebase.initializeApp(config);
